0bdb5947240ce91ba50750d7f0d7d8ad68136a89
adhishtanaka <kulasoooriyaa@gmail.com>
2026-01-30 23:17:22 +0530

    feat(auth): added auth services related configs

--- backend/auth-service/src/main/java/com/disa/auth_service/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/disa/auth_service/AuthServiceApplication.java
@@ -1,5 +1,6 @@
 package com.disa.auth_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
