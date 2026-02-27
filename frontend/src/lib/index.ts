497671e98ad5863531fa2b1eb25f0c1cb2220b45
Nethmina Kavinda <nkbandara@students.nsbm.ac.lk>
2026-02-27 12:33:11 +0530

    feat(assesment-service)

--- frontend/src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as api, assessmentAPI } from './api'
-export { useAssessmentStore } from './assessmentStore'
-export type { Assessment, AssessmentPhoto } from './assessmentStore'
-export { cn } from './utils'
